d11a162dedf7a42e61bd62af2e44baf0529402b5
Mukul Agarwal <agarmukul23@gmail.com>
2023-10-26 05:15:09 -0400

    fix up proof of concept

--- src/computer_vision/hand_pose_detection/tensorflow_proofofconcept.py
+++ src/computer_vision/hand_pose_detection/tensorflow_proofofconcept.py
@@ -1,52 +1,7 @@
 # import libraries
 import cv2
 import mediapipe as mp
-from mediapipe import solutions
-from mediapipe.framework.formats import landmark_pb2
 import numpy as np
-
-MARGIN = 10  # pixels
-FONT_SIZE = 1
-FONT_THICKNESS = 1
-HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # vibrant green
-
-
-def draw_landmarks_on_image(rgb_image, detection_result):
-    hand_landmarks_list = detection_result.hand_landmarks
-    handedness_list = detection_result.handedness
-    annotated_image = np.copy(rgb_image)
-
-    # Loop through the detected hands to visualize.
-    for idx in range(len(hand_landmarks_list)):
-        hand_landmarks = hand_landmarks_list[idx]
-        handedness = handedness_list[idx]
-
-        # Draw the hand landmarks.
-        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-        hand_landmarks_proto.landmark.extend([
-            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
-        ])
-        solutions.drawing_utils.draw_landmarks(
-            annotated_image,
-            hand_landmarks_proto,
-            solutions.hands.HAND_CONNECTIONS,
-            solutions.drawing_styles.get_default_hand_landmarks_style(),
-            solutions.drawing_styles.get_default_hand_connections_style())
-
-        # Get the top left corner of the detected hand's bounding box.
-        height, width, _ = annotated_image.shape
-        x_coordinates = [landmark.x for landmark in hand_landmarks]
-        y_coordinates = [landmark.y for landmark in hand_landmarks]
-        text_x = int(min(x_coordinates) * width)
-        text_y = int(min(y_coordinates) * height) - MARGIN
-
-        # Draw handedness (left or right hand) on the image.
-        cv2.putText(annotated_image, f"{handedness[0].category_name}",
-                    (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                    FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
-
-    return annotated_image
-
 
 # Video reading code adapted from:
 # https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/
@@ -58,10 +13,15 @@ HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
+current_frame = 0
 
+current_result: list[HandLandmarkerResult] = []
 # Create a hand landmarker instance with the live stream mode:
 def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
-    print('hand landmarker result: {}'.format(result))
+    current_result.clear()
+    if len(result.handedness) != 0:
+        current_result.append(result)
+
 
 
 options = HandLandmarkerOptions(
@@ -74,18 +34,45 @@ def main():
     with HandLandmarker.create_from_options(options) as landmarker:
         # get video capture
         video_capture = cv2.VideoCapture(0)
-        fps = video_capture.get(cv2.CAP_PROP_FPS)
-        ts = 0.0
+        frame_count = 0
         while True:
             # get next frame
             ret, frame = video_capture.read()
+            frame_count += 1
             frame = cv2.flip(frame, 1)
-            ts += 1000 / fps
             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-            landmarker.detect_async(mp_image, int(ts))
-
+            landmarker.detect_async(mp_image, frame_count)
             # display the frame
-            # cv2.imshow('frame', cv2.cvtColor(ann, cv2.COLOR_RGB2BGR))
+            frame = np.copy(frame)
+            height = len(frame)
+            width = len(frame[0])
+            frame = cv2.putText(frame, "Frame {}".format(frame_count), (10, 50), cv2.QT_FONT_NORMAL, 1, (0, 0, 255), 1, cv2.LINE_AA)
+            if len(current_result) != 0:
+                dy = 50
+                y = 50
+                result = current_result[0]
+                for hands in result.handedness:
+                    y += dy
+                    category = hands[0]
+                    name = category.category_name
+                    score = int(category.score * 100)
+                    output = "Found {} hand ({}%)".format(name, score)
+                    frame = cv2.putText(
+                        frame,
+                        output,
+                        (10, y),
+                        cv2.QT_FONT_NORMAL,
+                        1,
+                        (255, 0, 0),
+                        1,
+                        cv2.LINE_AA,
+                    )
+                for landmark in result.hand_landmarks[0]:
+                    x = int(width * landmark.x)
+                    y = int(height * landmark.y)
+                    frame = cv2.circle(frame, (x, y), radius=5, color=(0, 255, 255), thickness=-1)
+
+            cv2.imshow('frame', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         # clean up
