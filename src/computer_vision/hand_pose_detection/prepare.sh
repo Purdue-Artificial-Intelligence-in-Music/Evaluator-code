5c764b20135c5b4142a83aaabb7c1ea5bd9d3518
Mukul Agarwal <agarmukul23@gmail.com>
2023-10-26 04:06:11 -0400

    update prepare.sh

--- src/computer_vision/hand_pose_detection/prepare.sh
+++ src/computer_vision/hand_pose_detection/prepare.sh
@@ -1,10 +1,14 @@
-# Installs the hand_landmarker model
+#!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo "$SCRIPT_DIR"
 cd ../../..
 if [ ! -d models ]; then
   mkdir models || exit 1
 fi
 cd models || exit 2
-wget 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task' \
-  -O hand_landmarker.task
+if [ -f hand_landmarker.task ]; then
+  echo "The model already exists";
+  exit 0;
+else
+  wget 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task' \
+    -O hand_landmarker.task
+fi
