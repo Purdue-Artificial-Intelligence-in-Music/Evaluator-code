f9ae58027231720d34a95df12714b37851e3a757
Mukul Agarwal <agarmukul23@gmail.com>
2023-11-29 18:08:16 -0500

    minor refactorings and comments

--- src/computer_vision/hand_pose_detection/tensorflow_proofofconcept.py
+++ src/computer_vision/hand_pose_detection/tensorflow_proofofconcept.py
@@ -7,15 +7,18 @@ from mediapipe.tasks.python.components.containers.landmark import NormalizedLand
 # Video reading code adapted from:
 # https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/
 
-
+# Base option setup
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
 HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
+# Global frame count
 current_frame = 0
 
+# A static dictionary which allows for the lookup of the
+# indices corresponding to each hand
 lines: dict[str, list[int]] = {
     "thumb": [0, 1, 2, 3, 4],
     "index": [0, 5, 6, 7, 8],
@@ -25,26 +28,51 @@ lines: dict[str, list[int]] = {
 }
 
 
-def telescoping(line: list[int]) -> list[(int, int)]:
+def telescoping(values: list[int]) -> list[(int, int)]:
+    """
+    Creates a `telescoping` version of the values passed in pairing each
+    value with its successor.
+    For example:
+    >>> telescoping([1,2,3])
+    [(1, 2), (2, 3)]
+
+    :param values: the list of values to convert to a telescoping list
+    :return: the telescoped version of `values`
+    """
     result = []
-    for i in range(0, len(line) - 1):
-        result.append((line[i], line[i + 1]))
+    for i in range(0, len(values) - 1):
+        result.append((values[i], values[i + 1]))
     return result
 
 
+# Pre-computed connections and nodes derived from `lines`
 connections = [pair for x in lines for pair in telescoping(lines[x])]
 nodes = [node for finger in lines for node in lines[finger]]
 
 
 def distance(a: (int, int), b: (int, int)) -> float:
+    """
+    Calculates the euclidian distance between two points
+    :param a: the first point
+    :param b: the second point
+    :return: the distance between a and b using Pythagoras' theorem
+    """
     ax, ay = a
     bx, by = b
     return ((ax - bx) ** 2 + (ay - by) ** 2) ** 0.5
 
 
-# essentially straightness is just sinuosity
 def straightness(points: [int], landmarks: list[(int, int)]) -> float:
-    # segment distances
+    """
+    Calculates the `straightness` of a segment of points
+    This straightness is essentially the same as sinuosity
+    (https://en.wikipedia.org/wiki/Sinuosity)
+
+    :param points: the indices of the landmarks to consider in order of the intended line to be formed
+    :param landmarks: the full list of points, some of which are considered
+    :return: the straightness of the line, in (0, 1], where the higher values indicate a straighter line formed
+    """
+    # select the relevant segments
     lm = [landmarks[i] for i in points]
     d_running = 0
     for i in range(0, len(points) - 1):
@@ -54,30 +82,49 @@ def straightness(points: [int], landmarks: list[(int, int)]) -> float:
     return dtot / d_running
 
 
+# Data structure for persistent storae of the result
+# TODO: probably convert this to a instance variable of a class
 current_result: list[HandLandmarkerResult] = []
 
 
-# Create a hand landmarker instance with the live stream mode:
+def handle_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int) -> None:
+    """
+    Handles the result of model by saving it into the global `current_result`
+    :param result: the result obtained
 
-def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    :param timestamp_ms: IGNORED (needed for calling convention)
+    :param output_image: IGNORED (needed for calling convention)
+    """
     current_result.clear()
     if len(result.handedness) != 0:
         current_result.append(result)
 
 
+# Setup options for the model
 options = HandLandmarkerOptions(
     base_options=BaseOptions(model_asset_path='../../../models/hand_landmarker.task'),
     num_hands=2,
     running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result)
+    result_callback=handle_result)
 
 
 def get_position(lm: NormalizedLandmark, width: int, height: int) -> (int, int):
+    """
+    Maps a landmark to a pixel
+    :param lm the landmark to use (with x,y in [0,1])
+    :param width: the image width
+    :param height: the image height
+    :return: the pixel position of the landmark
+    """
     x = int(width * lm.x)
     y = int(height * lm.y)
     return x, y
 
+
 def main():
+    """
+    Main function that sets up video feed, runs model, and displays livestream
+    """
     with HandLandmarker.create_from_options(options) as landmarker:
         # get video capture
         video_capture = cv2.VideoCapture(0)
