504c9e94398168a8db1452e7b871f6742b5ac538
JPKamphuis <jpkamphu@purdue.edu>
2024-01-27 15:44:00 -0500

    environment setup

--- src/computer_vision/hand_pose_detection/prepare.sh
+++ src/computer_vision/hand_pose_detection/prepare.sh
@@ -9,6 +9,8 @@ if [ -f hand_landmarker.task ]; then
   echo "The model already exists";
   exit 0;
 else
-  wget 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task' \
-    -O hand_landmarker.task
+  curl 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task' \
+    -o hand_landmarker.task
+  curl 'https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_heavy/float16/1/pose_landmarker_heavy.task' \
+    -o pose_landmarker.task
 fi
