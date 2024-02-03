2fd339e7ee728245ab1a51f54fea750562dd5522
Brian Qi <qi116@purdue.edu>
2024-02-03 17:16:08 -0500

    Printing note names

--- src/score/AudioThreadTest.py
+++ src/score/AudioThreadTest.py
@@ -2,7 +2,7 @@ from AudioThreadWithBufferPorted import *
 
 from librosa import *
 import matplotlib.pyplot as plt
-
+import math
 
 def test(arg):
     #print(type(arg[0]))
@@ -10,9 +10,17 @@ def test(arg):
     #numpy_array = np.frombuffer(arg, dtype=np.float64)
     f0, voiced_flag, voiced_probs = pyin(y=arg,
                                              fmin=note_to_hz('C2'),
-                                             fmax=note_to_hz('C7'))
+                                             fmax=note_to_hz('C7'), sr=44100)
+    if (len(f0) > 0):
+        f0 = f0[~np.isnan(f0)]
+    # for num in f0:
+    #     print(type(num))    
+        
     times = times_like(f0)
-    print("librosa out: ", f0)
+    if len(f0) > 0:
+        notes = hz_to_note(f0)
+    print("librosa freq: ", f0)
+    print("librosa out: ", notes)
 
     return
 def main():
