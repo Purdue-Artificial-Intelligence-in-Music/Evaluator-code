739b22800039973df3e2d1cc81cc49caf024d445
Brian Qi <qi116@purdue.edu>
2024-02-12 18:56:08 -0500

    Added check for rests and improved coding style for Code Review

--- src/score/AudioThreadTest.py
+++ src/score/AudioThreadTest.py
@@ -1,3 +1,9 @@
+'''
+Testing different libraries to generate a dataframe from microphone input
+Using PyAudio and Librosa. Using rms to calculate when a new note or rest occurs
+
+'''
+
 from AudioThreadWithBufferPorted import *
 import pyaudio
 from librosa import *
@@ -9,6 +15,11 @@ from music21 import *
 import numpy
 import pandas as pd
 
+'''
+callback function calls this and passes the most recent parts of the buffer
+Will be useful to calculate frequencies and rms in real time.
+
+'''
 def test(arg):
     a = 0
     a += 1
@@ -32,6 +43,13 @@ def test(arg):
     #     notes = hz_to_note(frequency)
     #     print("crepe out: ", notes)
     return
+
+'''
+main function to create and run Audio Thread.
+calculates frequencies and rms after Audio Thread finishes.
+contents will be organized into functions once we achieve desired results.
+
+'''
 def main():
     
     #Create and start PyAudio thread
@@ -43,6 +61,7 @@ def main():
         while True:
             print(my_thread.input_on)
             time.sleep(1.5)
+            #only let thread run for 5 seconds
             if time.time() - start > 5:
                 break
                 
@@ -58,28 +77,23 @@ def main():
                                              fmax=note_to_hz('C7'), sr=44100)
     #out = get_duration(y=buffer, sr=44100)
     
+    #replace NaN with 0s
     if (len(f0) > 0):
-        f0 = f0[~np.isnan(f0)]
-    
+        f0 = np.nan_to_num(f0, nan=0, posinf=10000, neginf=-10000)
+        
+    #get the time each entry is recorded    
     times = times_like(f0)
-    notes = []
-    if len(f0) > 0:
-        notes = hz_to_note(f0)
-        print("librosa freq: ", f0)
-        print("librosa out: ", notes)
     
-    # rms = feature.rms(y=buffer)
-    # x = np.arange(0, len(rms[0]))
-    # print(len(rms[0]))
-    # print("rms: ", rms)
-    # plt.title("Line graph") 
-    # plt.xlabel("X axis") 
-    # plt.ylabel("Y axis") 
-    # plt.plot(x, rms[0], color ="green") 
-    # plt.show()
+    notes = note_names_from_freqs(f0, 1.23)
+    
+    #The points in the array where a new note begins
     onset_frames = onset.onset_detect(y=buffer, sr=44100)
     print('onset_frames:', onset_frames)
+    
+    #Calculates RMS value of each entry
     rms = feature.rms(y=buffer)
+    
+    #Graph rms
     x = np.arange(0, len(rms[0]))
     print(len(rms[0]))
     print("rms: ", rms)
@@ -96,6 +110,23 @@ def main():
     df = pd.DataFrame(data=my_dict)
     print(df)
 
+
+'''
+Calculate the Note corresponding to the frequency 
+Will set note name to Rest if frequency is below a certain frequency
+Will return an array of note names corresponding to each entry in given array
+'''
+def note_names_from_freqs(f0: np.ndarray, rest_threshold:int=0):
+    notes = []
+    if len(f0) > 0:
+        for freq in f0:
+            if freq <= rest_threshold:
+                notes.append('Rest')
+            else:
+                notes.append(hz_to_note(freq))
+        print("librosa freq: ", f0)
+        print("librosa out: ", notes)
+    return notes
 
 if __name__ == "__main__":
     main()
