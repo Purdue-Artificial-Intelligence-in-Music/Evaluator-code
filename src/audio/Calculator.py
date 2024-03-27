256b809ea9cc228279e6c8d4b0a13184c746a988
Brian Qi <qi116@purdue.edu>
2024-03-27 12:10:06 -0400

    settings

--- src/audio/Calculator.py
+++ src/audio/Calculator.py
@@ -59,7 +59,6 @@ class Calculator:
         onset_env = onset.onset_strength(y=buffer, sr=44100,
                                          hop_length=512)
         
-        print("length", len(onset_env))
         onset_frames = util.peak_pick(onset_env, pre_max=3, post_max=3, pre_avg=3, post_avg=5, delta=0.5, wait=10)
         #onset_frames = onset.onset_detect(y=buffer, sr=44100, hop_length=512)
         
