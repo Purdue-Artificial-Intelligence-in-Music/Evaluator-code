e23ff8b3bf5c6f6e3e9f4c28968483650c5e1b44
WilliamJiang11 <Wpj11@hotmail.com>
2024-07-12 22:01:15 -0400

    Specified main method

--- src/computer_vision/hand_pose_detection/test.py
+++ src/computer_vision/hand_pose_detection/test.py
@@ -67,6 +67,7 @@ def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
         dim = (width, int(h * r))
  
     return cv2.resize(image, dim, interpolation=inter)
+
 def store_finger_node_coords(id: int, cx: float, cy: float):
   finger_coords = {}
 
@@ -80,160 +81,166 @@ def store_finger_node_coords(id: int, cx: float, cy: float):
     finger_coords[id] = []
   finger_coords[id].append((cx, cy))
 
-model = YOLO('/Users/Wpj11/Documents/GitHub/Evaluator-code/src/computer_vision/hand_pose_detection/best-2 1.pt')  # Path to your model file
-# For webcam input:
-# model.overlap = 80
-video_file_path = '/Users/Wpj11/Documents/GitHub/Evaluator-code/src/computer_vision/hand_pose_detection/Too much pronation (1).mp4'
-cap = cv2.VideoCapture(video_file_path)
+def main():
+  model = YOLO('/Users/Wpj11/Documents/GitHub/Evaluator-code/src/computer_vision/hand_pose_detection/best-2 1.pt')  # Path to your model file
+  # For webcam input:
+  # model.overlap = 80
 
-mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
+  #input file
+  video_file_path = '/Users/Wpj11/Documents/GitHub/Evaluator-code/src/computer_vision/hand_pose_detection/Too much pronation (1).mp4'
+  cap = cv2.VideoCapture(video_file_path)
 
-with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands, mp_pose.Pose(
-    model_complexity=0,
-    min_detection_confidence=0.4,
-    min_tracking_confidence=0.6) as pose:
+  mp_drawing = mp.solutions.drawing_utils
+  mp_pose = mp.solutions.pose
+  mp_drawing_styles = mp.solutions.drawing_styles
+  mp_hands = mp.solutions.hands
 
-
-  writer = cv2.VideoWriter("demo.avi", cv2.VideoWriter_fourcc(*"MJPG"), 12.5,(640,480)) # algo makes a frame every ~80ms = 12.5 fps
-  while cap.isOpened():
-    success, image = cap.read()
-    #if not success:
-      #print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      #break
- 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-    pose_results = pose.process(image)
-    hand_node_positions = []
-    # yolov8 prediction
-    YOLOresults = model(image)
-    for result in YOLOresults:
-        obb = result.obb
-        # if obb is not None:
-        #   coords = obb.xyxyxyxy
-        #   print("Box 1:", obb.xyxyxyxy)
-        #   print("Box 2:", (obb.xyxyxyxy)[0])
-        text = ""
-        # for i in range(len(result.obb.xyxyxyxy)):
-        #   coords = result.obb.xyxyxyxy[i]  # Accessing xyxyxyxy property to get the coordinates
-        #   # text = text + "\n" + f"Box {i+1}: ({coords})"
-        #   coords_str = ", ".join(map(str, coords))
-        #   text += f"Box {i + 1}: ({coords_str})\n"
-        #   print(f"Box {i + 1} coordinates:")
-        #   print(coords)
-        # max_text_width = 100  # Adjust based on your image and font size
-        # wrapped_text = wrap_text(text, max_text_width)
-        
-        if (len(result.obb.xyxyxyxy) > 0):
-          coord_box_one = result.obb.xyxyxyxy[0]
-          text_one = f"Bow OBB coords: ({coord_box_one})"
-          print("Bow OBB coords:", coord_box_one)
-          bottom_left_corner = (20, image.shape[0] - 30)
-          cv2.putText(image, text_one,  bottom_left_corner, cv2.FONT_HERSHEY_SIMPLEX, 1.3, (98, 0, 158), 4)
-        if (len(result.obb.xyxyxyxy) >= 2):
-          coord_box_two = result.obb.xyxyxyxy[1]
-          # TA (Target Area)
-          text_two = f"TA OBB coords: ({coord_box_two})"
-          print("TA OBB coords", coord_box_two)
-          top_right_corner = (20, image.shape[1] + 100)
-          cv2.putText(image, text_two, top_right_corner, cv2.FONT_HERSHEY_SIMPLEX, 1.3, (255, 0, 0), 4)
+  with mp_hands.Hands(
+      model_complexity=0,
+      min_detection_confidence=0.5,
+      min_tracking_confidence=0.5) as hands, mp_pose.Pose(
+      model_complexity=0,
+      min_detection_confidence=0.4,
+      min_tracking_confidence=0.6) as pose:
 
 
-    # for result in YOLOresults:
-    #   if result.obb:
-    #     obb = result.obb
-    #     for i in range(len(obb.xyxyxyxy)):
-    #       coords = obb.xyxyxyxy[i]
-    #       # Format coordinates text
-    #       text = f"Box {i + 1}: ({coords[0]}, {coords[1]}), ({coords[2]}, {coords[3]}), ({coords[4]}, {coords[5]}), ({coords[6]}, {coords[7]})"
-    #       # Draw text on the image
-    #     cv2.putText(image, text, (20, image.shape[0] - 30 - i * 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-    #     #  print("Box 2:", obb.xyxyxyxy[1])
-    #     #  print(obb.id)
-    detections = sv.Detections.from_ultralytics(YOLOresults[0])
-    # print("Detections:", detections)
+    writer = cv2.VideoWriter("demo.avi", cv2.VideoWriter_fourcc(*"MJPG"), 12.5,(640,480)) # algo makes a frame every ~80ms = 12.5 fps
+    while cap.isOpened():
+      success, image = cap.read()
+      #if not success:
+        #print("Ignoring empty camera frame.")
+        # If loading a video, use 'break' instead of 'continue'.
+        #break
+  
+      # To improve performance, optionally mark the image as not writeable to
+      # pass by reference.
+      image.flags.writeable = False
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+      results = hands.process(image)
+      pose_results = pose.process(image)
+      hand_node_positions = []
+      # yolov8 prediction
+      YOLOresults = model(image)
+      for result in YOLOresults:
+          obb = result.obb
+          # if obb is not None:
+          #   coords = obb.xyxyxyxy
+          #   print("Box 1:", obb.xyxyxyxy)
+          #   print("Box 2:", (obb.xyxyxyxy)[0])
+          text = ""
+          # for i in range(len(result.obb.xyxyxyxy)):
+          #   coords = result.obb.xyxyxyxy[i]  # Accessing xyxyxyxy property to get the coordinates
+          #   # text = text + "\n" + f"Box {i+1}: ({coords})"
+          #   coords_str = ", ".join(map(str, coords))
+          #   text += f"Box {i + 1}: ({coords_str})\n"
+          #   print(f"Box {i + 1} coordinates:")
+          #   print(coords)
+          # max_text_width = 100  # Adjust based on your image and font size
+          # wrapped_text = wrap_text(text, max_text_width)
+          
+          if (len(result.obb.xyxyxyxy) > 0):
+            coord_box_one = result.obb.xyxyxyxy[0]
+            text_one = f"Bow OBB coords: ({coord_box_one})"
+            print("Bow OBB coords:", coord_box_one)
+            bottom_left_corner = (20, image.shape[0] - 30)
+            cv2.putText(image, text_one,  bottom_left_corner, cv2.FONT_HERSHEY_SIMPLEX, 1.3, (98, 0, 158), 4)
+          if (len(result.obb.xyxyxyxy) >= 2):
+            coord_box_two = result.obb.xyxyxyxy[1]
+            # TA (Target Area)
+            text_two = f"TA OBB coords: ({coord_box_two})"
+            print("TA OBB coords", coord_box_two)
+            top_right_corner = (20, image.shape[1] + 100)
+            cv2.putText(image, text_two, top_right_corner, cv2.FONT_HERSHEY_SIMPLEX, 1.3, (255, 0, 0), 4)
 
-# Iterate over each detection
-    # count = 0
-    # for detection in detections:
-    #     count += 1
-    #     # print(f"Detection {count}: {detection}")
-    #     # print("Detection structure:", type(detection), detection)
 
-    #     # Ensure detection is iterable and extract coordinates, confidence, and class
-    #     try:
-    #         xyxy = detection[0]  # Bounding box coordinates
-    #         conf = detection[2]  # Confidence score
-    #         cls = detection[3]  # Class ID
-            
-    #         # Ensure xyxy is a list or array before converting to int
-    #         xmin, ymin, xmax, ymax = map(int, xyxy)
-    #         print(f"Bounding Box {count}: xmin={xmin}, ymin={ymin}, xmax={xmax}, ymax={ymax}")
-    #         text = f'({xmin},{ymin}),({xmax},{ymax})'
-    #         cv2.putText(image, text, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+      # for result in YOLOresults:
+      #   if result.obb:
+      #     obb = result.obb
+      #     for i in range(len(obb.xyxyxyxy)):
+      #       coords = obb.xyxyxyxy[i]
+      #       # Format coordinates text
+      #       text = f"Box {i + 1}: ({coords[0]}, {coords[1]}), ({coords[2]}, {coords[3]}), ({coords[4]}, {coords[5]}), ({coords[6]}, {coords[7]})"
+      #       # Draw text on the image
+      #     cv2.putText(image, text, (20, image.shape[0] - 30 - i * 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+      #     #  print("Box 2:", obb.xyxyxyxy[1])
+      #     #  print(obb.id)
+      detections = sv.Detections.from_ultralytics(YOLOresults[0])
+      # print("Detections:", detections)
 
-    #     except Exception as e:
-    #         print(f"Error processing detection {count}: {e}")
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    image_height, image_width, _ = image.shape
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        node_positions = []
-        for ids, landmrk in enumerate(hand_landmarks.landmark):
-          # print(ids, landmrk)
-          cx, cy = landmrk.x * image_width, landmrk.y*image_height
- 
-          # calls function to store position of nodes
-          store_finger_node_coords(id, cx, cy)
-          # print("id:", ids, " x:", cx, " y:", cy)
-          # print("id type: ", type(ids), " x type: ", type(cx), " y type: ", type(cy))
-          # print (ids, cx, cy)
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
+  # Iterate over each detection
+      # count = 0
+      # for detection in detections:
+      #     count += 1
+      #     # print(f"Detection {count}: {detection}")
+      #     # print("Detection structure:", type(detection), detection)
 
-        landmark_subset = landmark_pb2.NormalizedLandmarkList(
-          landmark = pose_results.pose_landmarks.landmark[11:15]
-        )
-        mp_drawing.draw_landmarks(
-            image,
-            landmark_subset,
-            None,
-            mp_drawing.DrawingSpec(color=(255,0,0), thickness=10, circle_radius=6))
+      #     # Ensure detection is iterable and extract coordinates, confidence, and class
+      #     try:
+      #         xyxy = detection[0]  # Bounding box coordinates
+      #         conf = detection[2]  # Confidence score
+      #         cls = detection[3]  # Class ID
+              
+      #         # Ensure xyxy is a list or array before converting to int
+      #         xmin, ymin, xmax, ymax = map(int, xyxy)
+      #         print(f"Bounding Box {count}: xmin={xmin}, ymin={ymin}, xmax={xmax}, ymax={ymax}")
+      #         text = f'({xmin},{ymin}),({xmax},{ymax})'
+      #         cv2.putText(image, text, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-    oriented_box_annotator = sv.OrientedBoxAnnotator()
-    annotated_frame = oriented_box_annotator.annotate(
-        scene=image,
-        detections=detections
-    )
- 
-    #print("Annotated Frame: ", annotated_frame)
-    # Flip the image horizontally for a selfie-view display.
-    # cv2.namedWindow('Mediapipe Hands', cv2.WINDOW_NORMAL)
- 
-    #image = ResizeWithAspectRatio(image, height=800)
-    writer.write(image)
-    cv2.imshow('MediaPipe Hands', image)
-    #if cv2.waitKey(5) & 0xFF == 27:
-      #break
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
-cap.release()
-cv2.destroyAllWindows()
-# print("Hand node Values: ", hand_node_positions)
-# print(finger_coords)
+      #     except Exception as e:
+      #         print(f"Error processing detection {count}: {e}")
+      # Draw the hand annotations on the image.
+      image.flags.writeable = True
+      image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+      image_height, image_width, _ = image.shape
+      if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+          node_positions = []
+          for ids, landmrk in enumerate(hand_landmarks.landmark):
+            # print(ids, landmrk)
+            cx, cy = landmrk.x * image_width, landmrk.y*image_height
+  
+            # calls function to store position of nodes
+            store_finger_node_coords(id, cx, cy)
+            # print("id:", ids, " x:", cx, " y:", cy)
+            # print("id type: ", type(ids), " x type: ", type(cx), " y type: ", type(cy))
+            # print (ids, cx, cy)
+          mp_drawing.draw_landmarks(
+              image,
+              hand_landmarks,
+              mp_hands.HAND_CONNECTIONS,
+              mp_drawing_styles.get_default_hand_landmarks_style(),
+              mp_drawing_styles.get_default_hand_connections_style())
+
+          landmark_subset = landmark_pb2.NormalizedLandmarkList(
+            landmark = pose_results.pose_landmarks.landmark[11:15]
+          )
+          mp_drawing.draw_landmarks(
+              image,
+              landmark_subset,
+              None,
+              mp_drawing.DrawingSpec(color=(255,0,0), thickness=10, circle_radius=6))
+
+      oriented_box_annotator = sv.OrientedBoxAnnotator()
+      annotated_frame = oriented_box_annotator.annotate(
+          scene=image,
+          detections=detections
+      )
+  
+      #print("Annotated Frame: ", annotated_frame)
+      # Flip the image horizontally for a selfie-view display.
+      # cv2.namedWindow('Mediapipe Hands', cv2.WINDOW_NORMAL)
+  
+      image = ResizeWithAspectRatio(image, height=800)
+      writer.write(image)
+      cv2.imshow('MediaPipe Hands', image)
+      #if cv2.waitKey(5) & 0xFF == 27:
+        #break
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  cap.release()
+  cv2.destroyAllWindows()
+  # print("Hand node Values: ", hand_node_positions)
+  # print(finger_coords)
+  
+if __name__ == "__main__":
+    main()
