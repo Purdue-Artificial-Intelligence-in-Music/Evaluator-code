1a7446eab87e893581d49161d6340b00d192e89c
PaoloWang1 <paolo060314@gmail.com>
2025-08-05 08:10:35 -0500

    deleting uneccesary files

--- src/audio/test code/recording/BeatNetTest.py
+++
@@ -1,9 +0,0 @@
-from BeatNet.BeatNet import BeatNet
-
-estimator = BeatNet(1, mode='online', inference_model='DBN', plot=[], thread=False)
-
-Output = estimator.process('correct.wav')
-print(Output)
-print("exits")
-
-
