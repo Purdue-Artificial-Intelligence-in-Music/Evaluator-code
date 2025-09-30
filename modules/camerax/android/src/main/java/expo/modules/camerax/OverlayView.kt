234e31a87458bedfd354ea5667aeba3d83010f66
Paige Lorenz <pelorenz08@gmail.com>
2025-09-29 20:22:35 -0400

    hand detection

--- modules/camerax/android/src/main/java/expo/modules/camerax/OverlayView.kt
+++ modules/camerax/android/src/main/java/expo/modules/camerax/OverlayView.kt
@@ -5,6 +5,13 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.ContextCompat
+import com.google.mediapipe.tasks.vision.handlandmarker.HandLandmarker
+import com.google.mediapipe.tasks.vision.handlandmarker.HandLandmarkerResult
+import com.google.mediapipe.tasks.vision.poselandmarker.PoseLandmarker
+import com.google.mediapipe.tasks.vision.poselandmarker.PoseLandmarkerResult
+import com.google.mediapipe.tasks.vision.core.RunningMode
+import kotlin.math.min
+import kotlin.math.max
 
 
 import kotlin.text.toFloat
@@ -19,6 +26,20 @@ class OverlayView @JvmOverloads constructor(
     private var boxPaint : Paint = Paint()
     private var textPaint = Paint()
     private var anglePaint = Paint()
+
+    private var handLandmarkerResult: HandLandmarkerResult? = null
+    private var poseLandmarkerResult: PoseLandmarkerResult? = null
+
+    private var linePaint = Paint()
+    private var pointPaint = Paint()
+    private var poseLinePaint = Paint()
+    private var posePointPaint = Paint()
+    private var testPaint = Paint()
+
+    // New properties to store the offsets
+    private var xOffset: Float = 0f
+    private var yOffset: Float = 0f
+    private var handsScaleFactor = 0f
 
     companion object {
         // Classification constants
@@ -46,6 +67,8 @@ class OverlayView @JvmOverloads constructor(
             ANGLE_RIGHT to "Correct Bow Angle",
             ANGLE_WRONG to "Incorrect Bow Angle"
         )
+
+        const val LANDMARK_STROKE_WIDTH = 8f
     }
 
     init {
@@ -59,7 +82,28 @@ class OverlayView @JvmOverloads constructor(
 
         anglePaint.setColor(Color.GREEN)
         anglePaint.style = Paint.Style.FILL
-        anglePaint.textSize =48f
+        anglePaint.textSize = 48f
+
+        testPaint.setColor(Color.GREEN)
+        testPaint.style = Paint.Style.FILL
+        testPaint.textSize = 48f
+
+        linePaint.color =
+            Color.RED
+        linePaint.strokeWidth = LANDMARK_STROKE_WIDTH
+        linePaint.style = Paint.Style.STROKE
+
+        pointPaint.color = Color.YELLOW
+        pointPaint.strokeWidth = LANDMARK_STROKE_WIDTH
+        pointPaint.style = Paint.Style.FILL
+
+        poseLinePaint.color = Color.WHITE
+        poseLinePaint.strokeWidth = LANDMARK_STROKE_WIDTH
+        poseLinePaint.style = Paint.Style.STROKE
+
+        posePointPaint.color = Color.BLUE
+        posePointPaint.strokeWidth = LANDMARK_STROKE_WIDTH
+        posePointPaint.style = Paint.Style.FILL
     }
 
     fun returnDims() : Pair<Int, Int> {
@@ -72,6 +116,7 @@ class OverlayView @JvmOverloads constructor(
         //val scaleY = height.toFloat() / imageHeight
         val scaleX = 1f
         val scaleY = 1f
+        val scaleFactor = 1f //max(scaleX, scaleY)
         if (results?.classification != -2) {
             val stringBox = results?.string
             val bowBox = results?.bow
@@ -98,7 +143,7 @@ class OverlayView @JvmOverloads constructor(
                     , boxPaint)
             }
             if (bowBox != null) {
-                println("DRAWING BOW BOX")
+                //println("DRAWING BOW BOX")
                 canvas.drawLine(bowBox[0].x.toFloat() * scaleX,
                     bowBox[0].y.toFloat() * scaleY,
                     bowBox[1].x.toFloat() * scaleX,
@@ -137,17 +182,90 @@ class OverlayView @JvmOverloads constructor(
                 160f,
                 anglePaint
             )
+        }
 
+        //comment out pose detector, distracting on top of live feed. maybe use for skeleton later?
+        /*poseLandmarkerResult?.let { poseResult ->
+            if (poseResult.landmarks().isNotEmpty()) {
+                val landmarks = poseResult.landmarks()[0]
+
+                // Draw connections
+                PoseLandmarker.POSE_LANDMARKS.forEach { connection ->
+                    canvas.drawLine(
+                        // Apply the x and y offsets to all coordinates
+                        landmarks[connection.start()].x() * imageWidth * handsScaleFactor + xOffset,
+                        landmarks[connection.start()].y() * imageHeight * handsScaleFactor + yOffset,
+                        landmarks[connection.end()].x() * imageWidth * handsScaleFactor + xOffset,
+                        landmarks[connection.end()].y() * imageHeight * handsScaleFactor + yOffset,
+                        poseLinePaint
+                    )
+                }
+
+                // Draw points
+                for (normalizedLandmark in landmarks) {
+                    canvas.drawPoint(
+                        // Apply the x and y offsets
+                        normalizedLandmark.x() * imageWidth * handsScaleFactor + xOffset,
+                        normalizedLandmark.y() * imageHeight * handsScaleFactor + yOffset,
+                        posePointPaint
+                    )
+                }
+            }
+        }*/
+
+        // Draw Hand Landmarks
+        handLandmarkerResult?.let { handResult ->
+            for (landmarks in handResult.landmarks()) {
+                // Draw hand connections
+                HandLandmarker.HAND_CONNECTIONS.forEach { connection ->
+                    canvas.drawLine(
+                        // Apply the x and y offsets
+                        landmarks.get(connection!!.start()).x() * imageWidth * handsScaleFactor + xOffset,
+                        landmarks.get(connection.start()).y() * imageHeight * handsScaleFactor + yOffset,
+                        landmarks.get(connection.end()).x() * imageWidth * handsScaleFactor + xOffset,
+                        landmarks.get(connection.end()).y() * imageHeight * handsScaleFactor + yOffset,
+                        linePaint
+                    )
+                }
+
+                // Draw hand points
+                for (normalizedLandmark in landmarks) {
+                    canvas.drawPoint(
+                        // Apply the x and y offsets
+                        normalizedLandmark.x() * imageWidth * handsScaleFactor + xOffset,
+                        normalizedLandmark.y() * imageHeight * handsScaleFactor + yOffset,
+                        pointPaint
+                    )
+                }
+            }
         }
 
 
     }
 
-    fun updateResults(results: Detector.returnBow) {
+    fun updateResults(results: Detector.returnBow?,
+                      hands:  HandLandmarkerResult?,
+                      pose:   PoseLandmarkerResult? ) {
         this.results = results
+        this.handLandmarkerResult = hands
+        this.poseLandmarkerResult = pose
         invalidate()
-
     }
+
+    fun setImageDimensions(imgWidth: Int, imgHeight: Int) {
+
+
+        imageWidth = imgWidth
+        imageHeight = imgHeight
+
+        val scaleX = this.width.toFloat() / imageWidth.toFloat()
+        val scaleY = this.height.toFloat() / imageHeight.toFloat()
+
+        //need to be updated for non-live feed?
+        handsScaleFactor = min(scaleX, scaleY)
+    }
+
+
 
 
 }
